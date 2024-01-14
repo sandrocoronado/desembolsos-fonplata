a4894c989a45327a77725d7957dce70718e25fba
Alessandro Coronado Siles <alecoronadosiles@hotmail.com>
2024-01-14 03:52:36 +0000

    a

--- pages/0_Animation_Demo.py
+++ pages/0_Animation_Demo.py
@@ -1,84 +1,99 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from typing import Any
-
-import numpy as np
-
 import streamlit as st
-from streamlit.hello.utils import show_code
+import pandas as pd
+import numpy as np
+import threading
+import io
+
+def dataframe_to_excel_bytes(df):
+    output = io.BytesIO()
+    with pd.ExcelWriter(output, engine='openpyxl') as writer:
+        df.to_excel(writer, sheet_name='Sheet1')
+    excel_bytes = output.getvalue()
+    return excel_bytes
+
+# Configuración inicial
+LOGGER = st.logger.get_logger(__name__)
+_lock = threading.Lock()
+
+# URLs de las hojas de Google Sheets
+sheet_url_proyectos = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSHedheaRLyqnjwtsRvlBFFOnzhfarkFMoJ04chQbKZCBRZXh_2REE3cmsRC69GwsUK0PoOVv95xptX/pub?gid=2084477941&single=true&output=csv"
+sheet_url_operaciones = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSHedheaRLyqnjwtsRvlBFFOnzhfarkFMoJ04chQbKZCBRZXh_2REE3cmsRC69GwsUK0PoOVv95xptX/pub?gid=1468153763&single=true&output=csv"
+sheet_url_desembolsos = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSHedheaRLyqnjwtsRvlBFFOnzhfarkFMoJ04chQbKZCBRZXh_2REE3cmsRC69GwsUK0PoOVv95xptX/pub?gid=1657640798&single=true&output=csv"
+
+st.title("Análisis de Desembolsos por Proyecto")
+
+def load_data(url):
+    with _lock:
+        return pd.read_csv(url)
+    
+def clean_and_convert_to_float(monto_str):
+    if pd.isna(monto_str):
+        return np.nan
+    try:
+        # Asumiendo que 'monto_str' es una cadena, remover puntos de los miles y cambiar comas por puntos para decimales
+        cleaned_monto = monto_str.replace('.', '').replace(',', '.')
+        return float(cleaned_monto)
+    except ValueError:
+        # Si hay un error en la conversión, retorna NaN
+        return np.nan
 
 
-def animation_demo() -> None:
+def process_data(df_proyectos, df_operaciones, df_operaciones_desembolsos):
+    # Aplicar la función de limpieza a la columna 'Monto'
+    df_operaciones_desembolsos['Monto'] = df_operaciones_desembolsos['Monto'].apply(clean_and_convert_to_float)
+    df_operaciones['AporteFONPLATAVigente'] = df_operaciones['AporteFONPLATAVigente'].apply(clean_and_convert_to_float)
 
-    # Interactive Streamlit elements, like these sliders, return their value.
-    # This gives you an extremely simple interaction model.
-    iterations = st.sidebar.slider("Level of detail", 2, 20, 10, 1)
-    separation = st.sidebar.slider("Separation", 0.7, 2.0, 0.7885)
+    df_proyectos = df_proyectos[['NoProyecto', 'IDAreaPrioritaria','AreaPrioritaria','IDAreaIntervencion','AreaIntervencion']]
+    df_operaciones = df_operaciones[['NoProyecto', 'NoOperacion', 'IDEtapa', 'Alias', 'Pais', 'FechaVigencia', 'Estado', 'AporteFONPLATAVigente']]
+    df_operaciones_desembolsos = df_operaciones_desembolsos[['IDDesembolso', 'IDOperacion', 'Monto', 'FechaEfectiva']]
 
-    # Non-interactive elements return a placeholder to their location
-    # in the app. Here we're storing progress_bar to update it later.
-    progress_bar = st.sidebar.progress(0)
+    merged_df = pd.merge(df_operaciones_desembolsos, df_operaciones, left_on='IDOperacion', right_on='IDEtapa', how='left')
+    merged_df = pd.merge(merged_df, df_proyectos, on='NoProyecto', how='left')
 
-    # These two elements will be filled in later, so we create a placeholder
-    # for them using st.empty()
-    frame_text = st.sidebar.empty()
-    image = st.empty()
+    merged_df['FechaEfectiva'] = pd.to_datetime(merged_df['FechaEfectiva'], dayfirst=True, errors='coerce')
+    merged_df['FechaVigencia'] = pd.to_datetime(merged_df['FechaVigencia'], dayfirst=True, errors='coerce')
+    merged_df['Ano'] = ((merged_df['FechaEfectiva'] - merged_df['FechaVigencia']).dt.days / 366).fillna(-1)
+    merged_df['Ano'] = merged_df['Ano'].astype(int)
+    
+    # Convierte las columnas 'Monto' y 'AporteFONPLATAVigente' a numéricas
+    merged_df['Monto'] = pd.to_numeric(merged_df['Monto'], errors='coerce')
+    merged_df['AporteFONPLATAVigente'] = pd.to_numeric(merged_df['AporteFONPLATAVigente'], errors='coerce')
+    
+    merged_df['Porcentaje'] = ((merged_df['Monto'] / merged_df['AporteFONPLATAVigente']) * 100).round(2)
+    merged_df['Monto'] = (merged_df['Monto']/1000).round(0)
+    st.write(merged_df)
 
-    m, n, s = 960, 640, 400
-    x = np.linspace(-m / s, m / s, num=m).reshape((1, m))
-    y = np.linspace(-n / s, n / s, num=n).reshape((n, 1))
+    resumen_df = merged_df.groupby('IDAreaPrioritaria').agg(
+    Proyectos=('IDEtapa', 'nunique'),  # Cuenta el número único de IDEtapa
+    Suma_Monto=('Monto', 'sum')        # Suma de Monto
+).reset_index()
 
-    for frame_num, a in enumerate(np.linspace(0.0, 4 * np.pi, 100)):
-        # Here were setting value for these two elements.
-        progress_bar.progress(frame_num)
-        frame_text.text("Frame %i/100" % (frame_num + 1))
+    # Agregar fila de Totales a resumen_df
+    total_proyectos = resumen_df['Proyectos'].sum()
+    total_suma_monto = resumen_df['Suma_Monto'].sum()
+    total_resumen_df = pd.DataFrame({'IDAreaPrioritaria': ['Total'], 'Proyectos': [total_proyectos], 'Suma_Monto': [total_suma_monto]})
+    resumen_df = pd.concat([resumen_df, total_resumen_df], ignore_index=True)
+    st.write(resumen_df)
+    
+    # Creación del resumen por área de intervención
+    resumen_intervencion_total_df = merged_df.groupby('IDAreaIntervencion').agg(
+        Proyectos_Unicos=('IDEtapa', 'nunique'),
+        Suma_Monto=('Monto', 'sum')
+    ).reset_index()
 
-        # Performing some fractal wizardry.
-        c = separation * np.exp(1j * a)
-        Z = np.tile(x, (n, 1)) + 1j * np.tile(y, (1, m))
-        C = np.full((n, m), c)
-        M: Any = np.full((n, m), True, dtype=bool)
-        N = np.zeros((n, m))
+    # Agregar fila de Totales a resumen_intervencion_total_df
+    total_proyectos_unicos = resumen_intervencion_total_df['Proyectos_Unicos'].sum()
+    total_suma_monto_intervencion = resumen_intervencion_total_df['Suma_Monto'].sum()
+    total_resumen_intervencion_df = pd.DataFrame({'IDAreaIntervencion': ['Total'], 'Proyectos_Unicos': [total_proyectos_unicos], 'Suma_Monto': [total_suma_monto_intervencion]})
+    resumen_intervencion_total_df = pd.concat([resumen_intervencion_total_df, total_resumen_intervencion_df], ignore_index=True)
+    st.write(resumen_intervencion_total_df)
 
-        for i in range(iterations):
-            Z[M] = Z[M] * Z[M] + C[M]
-            M[np.abs(Z) > 2] = False
-            N[M] = i
+    return merged_df[merged_df['Ano'] >= 0]
 
-        # Update the image placeholder by calling the image() function on it.
-        image.image(1.0 - (N / N.max()), use_column_width=True)
+df_proyectos = load_data(sheet_url_proyectos)
+df_operaciones = load_data(sheet_url_operaciones)
+df_operaciones_desembolsos = load_data(sheet_url_desembolsos)
 
-    # We clear elements by calling empty on them.
-    progress_bar.empty()
-    frame_text.empty()
-
-    # Streamlit widgets automatically run the script from top to bottom. Since
-    # this button is not connected to any other logic, it just causes a plain
-    # rerun.
-    st.button("Re-run")
+processed_data = process_data(df_proyectos, df_operaciones, df_operaciones_desembolsos)
 
 
-st.set_page_config(page_title="Animation Demo", page_icon="📹")
-st.markdown("# Animation Demo")
-st.sidebar.header("Animation Demo")
-st.write(
-    """This app shows how you can use Streamlit to build cool animations.
-It displays an animated fractal based on the the Julia Set. Use the slider
-to tune different parameters."""
-)
-
-animation_demo()
-
-show_code(animation_demo)
